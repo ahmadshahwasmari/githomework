a8c5a48687400a6a7060513eece6413181fa0af0
shah.asmari@gmail.com <shah.asmari@gmail.com>
2021-10-27 21:36:10 -0400

    third commit

---
+++ src/com/Syntax/class1/gittext.java
@@ -0,0 +1,10 @@
+package com.Syntax.class1;
+
+public class gittext {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/Syntax/class1/gittext1.java
@@ -0,0 +1,10 @@
+package com.Syntax.class1;
+
+public class gittext1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
