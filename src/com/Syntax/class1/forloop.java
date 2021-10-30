5787095609dc8e3ddcfa2e106832abbf676d7857
ahmadshah <shah.asmari@gmail.com>
2021-10-30 11:23:56 -0400

    last commit

--- src/com/Syntax/class1/forloop.java
+++ src/com/Syntax/class1/forloop.java
@@ -1,10 +1,16 @@
 package com.Syntax.class1;
 
-public class gittext {
+public class forloop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+for( int i=0; i>5; i++) {
+	
 
+	System.out.println();
+	
+}
 	}
 
 }
---
+++ src/com/Syntax/class1/git3.java
@@ -0,0 +1,29 @@
+package com.Syntax.class1;
+
+public class git3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+int number =4;
+switch(number) {
+case 1:
+	System.out.println("1");
+	break;
+	case 2:
+		System.out.println("2");
+		break;
+	case 3:
+		System.out.println("3");
+		break;
+		default:
+			System.out.println("some else");
+	
+	
+		
+		
+		
+}
+	}
+
+}
