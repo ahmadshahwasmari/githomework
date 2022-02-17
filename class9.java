bf4a0b6028bb6eb3ccaa2f28dd838c44ab32cb7e
ahmadshahwasmari <shah.asmari@gmail.com>
2022-02-16 20:09:37 -0500

    new changes

--- class9.java
+++ class9.java
@@ -1,9 +1,20 @@
 package com.Syntax.class1;
 
+import java.util.Scanner;
+
 public class class9 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+Scanner scan= new Scanner(System.in);
+System.out.println("enter your name");
+String name=scan.next();
+System.out.println("enter your address");
+String address=scan.next();
+System.out.println("Enter your postal code");
+String postalcode=scan.next();
+System.out.println("your name is " +name+ " your address is " +address+ " and Postal code is " +postalcode);
+System.out.println("Great");
 
 	}
 
