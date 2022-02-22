6acf8a472493e338fafa900d57a1a548787b22e6
ahmadshahwasmari <shah.asmari@gmail.com>
2022-02-22 15:53:31 -0500

    evening updates

--- class19.java
+++ class19.java
@@ -5,6 +5,7 @@ public class class19 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello how are you");
+System.out.println("i am fine and you");
 	}
 
 }
